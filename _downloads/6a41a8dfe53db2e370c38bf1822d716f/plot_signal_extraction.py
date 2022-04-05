45531b04793961674e7841e300c3ba95f6665b48
alexis <alexisthual@gmail.com>
2022-04-05 18:08:47 +0200

    Revert "Update docs"
    This reverts commit 37735c98d2b462286243e5686730df17982922dd.

--- _downloads/6a41a8dfe53db2e370c38bf1822d716f/plot_signal_extraction.py
+++ _downloads/6a41a8dfe53db2e370c38bf1822d716f/plot_signal_extraction.py
@@ -22,9 +22,6 @@ NeuroImage 2013
 
 This is just a code example, see the :ref:`corresponding section in the
 documentation <parcellation_time_series>` for more.
-
-.. note::
-    This example needs SciPy >= 1.0.0 for the reordering of the matrix.
 """
 
 ##############################################################################
@@ -254,4 +251,4 @@ plotting.show()
 # References
 # ----------
 #
-#  .. footbibliography::
+# .. footbibliography::
